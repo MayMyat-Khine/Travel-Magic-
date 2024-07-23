88f7c0a5eb314981f5a1785afc726b3f9bd57035
MayMyat <khinemyat312@gmail.com>
2024-07-23 15:28:24 +0630

    prepare table view data scouce

--- TravelMagic/ViewController.swift
+++ TravelMagic/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var countriesTableView: UITableView?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableView = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return tableView
+    }
+}
