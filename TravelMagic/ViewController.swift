b82d5a62451fbc90d6032f0c99422f498cbeded7
MayMyat <khinemyat312@gmail.com>
2024-07-23 17:25:05 +0630

    prepare tableview cell and delegate for the cell

--- TravelMagic/ViewController.swift
+++ TravelMagic/ViewController.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var countriesTableView: UITableView?
+
+    let countries = ["China", "Myanmar", "India", "HongKong", "Thai", "Maxico", "Paris", "United Kingdom", "Japan"]
+    enum Constants {
+        static var callIdentifier = "MagicTableCell"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countriesTableView?.register(UITableViewCell.self, forCellReuseIdentifier: Constants.callIdentifier)
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return countries.count
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -25,7 +32,18 @@ extension ViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableView = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        return tableViewv
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: Constants.callIdentifier, for: indexPath)
+
+        var cellContantConfiguratin = tableViewCell.defaultContentConfiguration()
+        cellContantConfiguratin.text = countries[indexPath.row]
+        tableViewCell.contentConfiguration = cellContantConfiguratin
+        return tableViewCell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("User ON Tapped index \(indexPath)")
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
